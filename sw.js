5541831e50fb956a76ad0e4586e1e8700d545ea0
sami638341 <urrehmansami596@gmail.com>
2026-07-17 22:15:48 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,21 @@
-const CACHE_NAME = 'fitforge-v2';
-const urlsToCache = [
-  '.',
-  'index.html',
-  'manifest.json'
-];
+const CACHE_NAME = 'fitforge-v3'; // new version
 
 self.addEventListener('install', event => {
+  self.skipWaiting(); // activate immediately
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(['.', 'index.html', 'manifest.json']);
+    })
   );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-    ))
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    }).then(() => self.clients.claim()) // take control of all pages
   );
 });
 
